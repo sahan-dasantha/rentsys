a6cc4814fbce54ed60b0152807e5cc7e5b21c330
Sahan Dasantha <as2022502@sci.sjp.ac.lk>
2026-05-20 18:40:17 +0530

    Created the property model

--- Backend/demo/src/main/java/com/example/demo/model/Property.java
+++ Backend/demo/src/main/java/com/example/demo/model/Property.java
@@ -1,5 +1,44 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "property")
 public class Property {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "property_id")
+    private Long propertyId;
+
+    @Column(name = "address", length = 200)
+    private String address;
+
+    @Column(name = "type", length = 50)
+    private String type;        //house,apartment,villa
+    
+    @Column(name = "rent_amount")
+    private Double rentAmount;   //available,occupied
+
+    @Column(name = "status", length = 20)
+    private String status;  //available,occupied
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
 }
