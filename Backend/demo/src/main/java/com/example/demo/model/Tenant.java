1ee2a19275c1ce85a372396f170dddf00f3bc9a0
Sahan Dasantha <as2022502@sci.sjp.ac.lk>
2026-05-23 12:41:23 +0530

    Tenant model was added

--- Backend/demo/src/main/java/com/example/demo/model/Tenant.java
+++ Backend/demo/src/main/java/com/example/demo/model/Tenant.java
@@ -1,5 +1,48 @@
 package com.example.demo.model;
 
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tenant")
+
 public class Tenant {
-    
+ 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "recident_id", length = 200)
+    private Long residentId;
+
+    @Column(name = "full_name", length = 200)
+    private String fullName;
+
+    @Column(name = "national_id", length = 50)
+    private String nationalId;
+
+    @Column(name = "phone_number", length = 100)
+    private String phoneNumber;
+
+    @Column(name = "email", length = 200)
+    private String email;
+
+    @Column(name = "occupation", length = 100)
+    private String occupation;
+
+    @Column(name = "date_registered")
+    private Date dateRegistered;
+
 }
